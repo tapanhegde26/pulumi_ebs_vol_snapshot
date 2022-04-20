04527f0398dda7c3854580405a36e7f4eb998805
sheldon-cooper26 <55941193+sheldon-cooper26@users.noreply.github.com>
2022-04-20 13:18:26 +0530

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,21 +1,39 @@
 """An AWS Python Pulumi program"""
 
-import pulumi
 import pulumi_aws as aws
-import datetime;
+import datetime
+import logging
 
+logger = logging.getLogger(__name__)
 ct = datetime.datetime.now()
 ts = ct.timestamp()
-foo = aws.ec2.get_instance(instance_id="i-00590118d16b5ed8f")
-foo2 = aws.ec2.get_instance(instance_id="i-055c551c95629c394")
+source_instance = aws.ec2.get_instance(instance_id="i-07489eeacaa7bdf12")
+target_instance = aws.ec2.get_instance(instance_id="i-0a7667569757abf84")
 
-ebslist = foo.ebs_block_devices
-snapshot_name = "vol-snapshots"+"-"+str(ts)
+ebslist = source_instance.ebs_block_devices
+snapshot_name = "vol_snapshots"+"-"+str(ts)
 cloned_vol_name = "cloned_vol"+"-"+str(ts)
 vol_attachment = "vol_attachment"+"-"+str(ts)
-for i in ebslist:
-    print(i.volume_id)
-    foo_snapshot = aws.ebs.Snapshot(snapshot_name+str(i.volume_id),volume_id=i.volume_id)
-    foo_cloned_volume = aws.ebs.Volume(cloned_vol_name+str(i.volume_id),availability_zone="us-east-1b",snapshot_id=foo_snapshot.id)
-    cloned_vol_attachment = aws.ec2.VolumeAttachment(vol_attachment+str(i.volume_id),device_name=i.device_name,volume_id=foo_cloned_volume.id,instance_id=foo2.instance_id)
+try:
+    for i in ebslist:
+        logger.info("Loop over EBS list to get volume-id and device_name")
+        try:
+         source_snapshot = aws.ebs.Snapshot(snapshot_name+str(i.volume_id),volume_id=i.volume_id)
+        except RuntimeError:
+            logger.exception("Snapshot creation failed")
+            raise
+        try:
+            source_cloned_volume = aws.ebs.Volume(cloned_vol_name+str(i.volume_id),availability_zone="us-east-1c",snapshot_id=source_snapshot.id)
+        except RuntimeError:
+            logger.exception("Volume creation from snapshots failed")
+            raise
+        try:
+            cloned_vol_attachment = aws.ec2.VolumeAttachment(vol_attachment+str(i.volume_id),device_name=i.device_name,volume_id=source_cloned_volume.id,instance_id=target_instance.instance_id)
+        except RuntimeError:
+            logger.exception("Volume attachment to target instance failed")
+            raise
+except RuntimeError:
+    logger.exception("Looping through EBS block list failed")
+    raise
+
 
